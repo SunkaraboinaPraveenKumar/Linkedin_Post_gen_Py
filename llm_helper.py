3cfddf81c52eca39526e01b558451dc1fa11946e
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-11-08 16:46:46 +0530

    linkedin_post_gen

---
+++ llm_helper.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+import os
+
+load_dotenv()
+
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"),model="llama-3.2-90b-text-preview")
+
+
+if __name__ == "__main__":
+    response = llm.invoke("What are ML and DL?")
+    print(response.content)
