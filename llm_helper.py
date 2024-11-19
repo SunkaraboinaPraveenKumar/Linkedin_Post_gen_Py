8fd6743b72b39fe3c73bd5190688f8a1b3e47c22
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-11-19 06:37:47 +0530

    changes

--- llm_helper.py
+++ llm_helper.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"),model="llama-3.2-90b-text-preview")
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"),model="llama-3.1-70b-versatile")
 
 
 if __name__ == "__main__":
